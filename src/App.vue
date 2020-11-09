ea2748ba6b48a6bf64eee504b85e23870549f964
nikitagaltsev <nikitagalcev@gmail.com>
2020-11-09 11:19:40 +0200

    project init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header class="header"><h1>Text editor on Vue.js</h1></header>
+    <hr />
+    <main>
+      <textarea class="textarea">
+
+      </textarea>
+    </main>
+    <footer class="footer">
+      <p>
+        {{ getYear() }} © Nikita Galtsev. Test task for Redentu.
+      </p>
+    </footer>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    getYear() {
+      return new Date().getFullYear();
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .header {
+    text-align: center;
+  }
+
+  .footer {
+    text-align: center;
+  }
+</style>
